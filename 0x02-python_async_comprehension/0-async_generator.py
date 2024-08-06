94e0804709fb1b3e2eacde6dbe4b47e5cc6ade59
shaunpetro <shaun.p.mckoy@gmail.com>
2024-08-06 11:30:10 +0200

    project files

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''MOdule for task 1
+'''
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''generates a random sequence of 10 num
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
+        
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''module for task 2
+'''
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
